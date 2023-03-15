67fd1db9311503f3c89db80bb1e6eb86b923f282
markziegler1 <mark893b@stud.kea.dk>
2023-03-15 14:24:17 +0100

    Login

--- js/login.js
+++ js/login.js
@@ -1,26 +1,32 @@
 function login() {
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
+    // Get the username and password from the login form
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
 
-    // You can add a server-side authentication process here
-
+    // Check if the username and password are correct
     if (username === "manager" && password === "123") {
-        alert("Login successful!");
-    window.location.href = "index.html"; }
-
-    else if (username === "operator" && password === "123") {
-        alert("Login successful!");
-        window.location.href = "index.html"; }
-
+        // Redirect the user to the new HTML page
+        window.location.href = "index.html";
+    }
     else if (username === "shop" && password === "123") {
-        alert("Login successful!");
-        window.location.href = "index.html"; }
-
+        window.location.href = "index.html";
+    }
+    else if (username === "operator" && password === "123") {
+        window.location.href = "index.html";
+    }
     else if (username === "ticket" && password === "123") {
-        alert("Login successful!");
-        window.location.href = "index.html"; }
-
+        window.location.href = "index.html";
+    }
     else {
         alert("Incorrect username or password.");
     }
 }
+
+fetch('http://localhost:8080/mywebapp/mymethod')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data); // Process the data returned by the method
+    })
+    .catch(error => {
+        console.error(error); // Handle any errors that occur
+    });
