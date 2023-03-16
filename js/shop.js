4e1908860bd07610e1aa0f14f2345058f9002ed1
markziegler1 <mark893b@stud.kea.dk>
2023-03-16 13:41:07 +0100

    Kontakt form er lavet og virker

--- js/shop.js
+++ js/shop.js
@@ -1,11 +1,11 @@
 var cart = [];
 var products = [
-    {id: 1, name: "Lille popcorn", price: 40},
-    {id: 2, name: "Mellem popcorn", price: 50},
-    {id: 3, name: "Stor popcorn", price: 60},
-    {id: 4, name: "Lille sodavand", price: 30},
-    {id: 5, name: "Mellem sodavand", price: 40},
-    {id: 6, name: "Stor sodavand", price: 50}
+    {id: 1, name: "Lille popcorn", price: 40, quantity: 10, counter: 0},
+    {id: 2, name: "Mellem popcorn", price: 50, quantity: 10, counter: 0},
+    {id: 3, name: "Stor popcorn", price: 60, quantity: 10, counter: 0},
+    {id: 4, name: "Lille sodavand", price: 30, quantity: 10, counter: 0},
+    {id: 5, name: "Mellem sodavand", price: 40, quantity: 10, counter: 0},
+    {id: 6, name: "Stor sodavand", price: 50, quantity: 10, counter: 0},
 ];
 
 function addToCart(productId) {
@@ -13,9 +13,14 @@ function addToCart(productId) {
         return p.id === productId;
     });
 
-    if (product) {
+    if (product && product.quantity > 0) {
         cart.push(product);
+        product.quantity--;
+        product.counter++;
         updateCart();
+        saveCart();
+    } else {
+        alert("Sorry, this product is out of stock.");
     }
 }
 
@@ -23,17 +28,16 @@ function updateCart() {
     var cartItems = document.getElementById("cart-items");
     var cartTotal = document.getElementById("cart-total");
     var total = 0;
-    //var total = 0;
 
     cartItems.innerHTML = "";
     cart.forEach(function(product) {
         var li = document.createElement("li");
-        li.innerHTML = product.name + " - kr: " + product.price; //kr per item
+        li.innerHTML = product.name + " - kr: " + product.price + " - Quantity: " + product.counter;
         cartItems.appendChild(li);
-        total += product.price;
+        total += product.price * product.counter;
     });
 
-    cartTotal.innerHTML = "- kr: " + total.toFixed(2); //total kr
+    cartTotal.innerHTML = "- kr: " + total.toFixed(2);
 }
 
 function checkout() {
@@ -43,11 +47,30 @@ function checkout() {
     }
 
     var total = cart.reduce(function(sum, product) {
-        return sum + product.price;
+        return sum + product.price * product.counter;
     }, 0);
 
     alert("Your total is kr: " + total.toFixed(2) + ". Thank you for shopping!");
+    cart.forEach(function(product) {
+        product.quantity += product.counter;
+        product.counter = 0;
+    });
     cart = [];
+    updateCart();
+    saveCart();
     window.location.href = "index.html";
-    //updateCart();
 }
+
+function saveCart() {
+    sessionStorage.setItem('cart', JSON.stringify(cart));
+}
+
+function loadCart() {
+    var savedCart = sessionStorage.getItem('cart');
+    if (savedCart) {
+        cart = JSON.parse(savedCart);
+        updateCart();
+    }
+}
+
+loadCart();
